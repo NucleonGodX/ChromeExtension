999cb261633e345057ff1adcd34a7c4dfe8dadd0
NucleonGodX <you@example.com>
2023-05-14 10:20:23 +0530

    Moving forward with the project

--- extension.js
+++ extension.js
@@ -1,11 +1,24 @@
+//ARRAY OF DATA
+let myLeads=[]
+
 //VARIABLES
 const buttonEl=document.getElementById('press')
 const inputEl=document.getElementById('input-el')
+const showEl=document.getElementById("show-btn")
+let ulEl=document.getElementById('ul-el')
 
 //FUNCTIONS
 function Pressed(){
-    console.log("has been pressed")
+    myLeads.push(inputEl.value)
+    inputEl.value=''
+
 }
 
+buttonEl.addEventListener("click", Pressed)
+function Show(){
+for(let i=0; i<myLeads.length; i++){
+    ulEl.innerHTML+="<li> "+ myLeads[i]+"</li>"
+}
+}
 
-buttonEl.addEventListener("click", Pressed())
+showEl.addEventListener("click", Show)
